2ac91c9f0c73c0feb4ed58f7455bff6517f16e58
Argus94 <marcooliveri94@gmail.com>
2020-09-09 21:41:12 +0000

    dd

--- Lezioni/Html-css/Lezione 6/dati.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $nome = $_POST['nome'];
-    $cognome = $_POST['cognome'];
-    $email = $_POST'email'];
-    $compleanno = $_POST'date'];
-    echo $nome \n $cognome \n $email \n $compleanno;
-?>
